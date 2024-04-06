0e600ddaf832bb4eaa6762efb80d52060438bfe2
nguyenhoangan03 <nguyenhoangan03.work@gmail.com>
2024-04-06 16:56:21 +0700

    Spring MVC Validation and Custom Validation

--- Spring&SpringBoot/06-Spring-MVC/01-thymeleafdemo-helloworld/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ Spring&SpringBoot/06-Spring-MVC/01-thymeleafdemo-helloworld/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -1,18 +1,24 @@
 package com.luv2code.springboot.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
     private String firstName;
     private String lastName;
-
     private String country;
 
+    private String favouriteLanguage;
+
+    private List<String> favouriteSystems;
     public Student() {
     }
 
-    public Student(String firstName, String lastName, String country) {
+    public Student(String firstName, String lastName, String country, String favouriteLanguage, List<String> favouriteSystems) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.country = country;
+        this.favouriteLanguage = favouriteLanguage;
+        this.favouriteSystems = favouriteSystems;
     }
 
     public String getFirstName() {
@@ -37,6 +43,22 @@ public class Student {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public String getFavouriteLanguage() {
+        return favouriteLanguage;
+    }
+
+    public void setFavouriteLanguage(String favouriteLanguage) {
+        this.favouriteLanguage = favouriteLanguage;
+    }
+
+    public List<String> getFavouriteSystems() {
+        return favouriteSystems;
+    }
+
+    public void setFavouriteSystems(List<String> favouriteSystems) {
+        this.favouriteSystems = favouriteSystems;
     }
 
     @Override
